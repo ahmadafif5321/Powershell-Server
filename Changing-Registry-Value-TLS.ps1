765497cca17f1d86493fc143ac45171a12556441
ahmad afif bin che mohamad nor <ahmadafif5321@gmail.com>
2022-10-05 12:49:29 +0800

    Add files via upload
    Changing Registry value for EventLogging to get more details for event viewer regarding TLS event.
    
    Value	Description
    0x0000	Do not log
    0x0001	Log error messages
    0x0002	Log warnings
    0x0003	Log warnings and error messages
    0x0004	Log informational and success events
    0x0005	Log informational, success events and error messages
    0x0006	Log informational, success events and warnings
    0x0007	Log informational, success events, warnings, and error messages (all log levels)
    
    For more details: https://docs.microsoft.com/en-US/troubleshoot/developer/webapps/iis/health-diagnostic-performance/enable-schannel-event-logging

---
+++ Changing-Registry-Value-TLS.ps1
@@ -0,0 +1,20 @@
+﻿#Change path based on the location of file. Alternative is using import-csv for excel file
+#Replace "file-path" with the location of file. Example: "C:\temp\SHX Server.txt"
+
+$Server_List = Get-Content "file-path"
+$Path = "HKLM:\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL"
+$valueName = "EventLogging"
+$Type = "DWORD"
+$Value = "4"
+$Credential = Get-Credential
+
+foreach ($server in $Server_List)
+{Invoke-Command -ComputerName $server -ScriptBlock{
+set-Itemproperty -Path $using:Path -Name $using:ValueName -Value $using:Value -Type $using:Type
+} -Credential $Credential}
+
+#If got error path not exist, check the 32bits or 64 as the path will be different. 
+# X86: "HKLM:\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL"
+# X64: "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\SecurityProviders\SCHANNEL"
+# Run the script in AM Machine (SV02060) cause other server have firewall restriction
+# Set-ItemProperty -Path "HKLM: \System\CurrentControlSet\Control\SecurityProviders\SCHANNEL" -Value "4" -Name "EventLogging" -Type DWord
