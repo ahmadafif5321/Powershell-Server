9813e62ac98cc3de9db7012ad509eaf78855637e
ahmad afif bin che mohamad nor <ahmadafif5321@gmail.com>
2022-10-05 23:10:59 +0800

    Add files via upload
    Simple powershell script to check list of servers if online or offline

---
+++ Check-List-Server-Online-Offline.ps1
@@ -0,0 +1,33 @@
+﻿#set strict mode best practice to detect variable not declare
+Set-StrictMode -version latest
+
+#set variable to create shortcut. Replace "file-path" with the location of file. Example: "C:\temp\Server.csv"
+$filePath= "file-path"
+
+#import csv to get data inside excel in the path.
+$serverlist = import-csv -Path $filePath -Delimiter ','
+
+#create foreach to run each of server and create condition
+
+#creating ArrayList to store output to the excel
+$export = [System.Collections.ArrayList]@()
+
+foreach($server in $serverlist){
+    #declare server name for ease to call later
+    $serverName = $server.ServerName
+
+    #variable $connection created to pass resuly to condition
+    $connection=Test-Connection $server.ServerName -Count 1
+
+    if($connection.StatusCode -eq 0){
+        Write-Output "$($serverName) is online"
+    }
+    else{
+        Write-Output "$($serverName) is offline"
+    }
+    #adding server into array export
+    [void]$export.Add($server)
+}
+
+#pipeline to tranfer output to the csv file
+$export | Export-Csv -Path $filePath -Delimiter ',' -NoTypeInformation
