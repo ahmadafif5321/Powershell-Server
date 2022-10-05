90f037f98ba3b62e5511f80fb1e525561a989265
ahmad afif bin che mohamad nor <ahmadafif5321@gmail.com>
2022-10-05 12:59:07 +0800

    Add files via upload
    Simple command line to check specific path to know if it exist.

---
+++ Check-path-if-exist.ps1
@@ -0,0 +1,12 @@
+﻿#Strictmode to get return of error instead of null if you not declare something
+Set-StrictMode -Version latest
+
+#Replace "file-path" with the location of file. Example: "C:\temp\Server.txt"
+$filepath = "file-path"
+
+if (test-path -Path $filepath){
+    Write-Output "the path exist correctly"}
+else {
+    Write-Output "The $filepath not exist"}
+
+
